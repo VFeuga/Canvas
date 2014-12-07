bd6afc7a1ca81ba478c2c9460d453d92b69c29dc
Loïc <loic.feuga@gmail.com>
2014-12-07 16:11:17 +0100

    test pendule kinectic

--- js/jquery/index_jquery.js
+++ js/jquery/index_jquery.js
@@ -1,44 +1,47 @@
-
-$(document).ready(function() {
-
-   /*$( "p" ).click(function() {
-        $( this ).slideUp();
-    });
-
-    $("#id_genere").hover(function(){
-        $(this).animate({"width":"300px","margin-left":"50px"},1000);
-    },function(){
-        $(this).animate({"width":"400px","margin-left":"0px"},1000);
-    });*/
-
-var c = document.getElementById("mon_canvas");
-var ctx = c.getContext("2d");
-	// Voile du bateau
-ctx.beginPath();      // Début du chemin
-ctx.moveTo(150,80);   // Le tracé part du point 150,80
-ctx.lineTo(300,230);  // Un segment est ajouté vers 300,230
-ctx.lineTo(150,230);  // Un segment est ajouté vers 150,230
-ctx.closePath();      // Fermeture du chemin
-ctx.fillStyle = "lightblue"; // Définition de la couleur de remplissage
-ctx.fill();           // Remplissage du dernier chemin tracé
-
-// Coque du bâteau
-ctx.beginPath();      // Début d'un autre chemin
-ctx.moveTo(50,250);
-ctx.lineTo(100,300);
-ctx.lineTo(250,300);
-ctx.lineTo(300,250);
-ctx.fillStyle = "peru";
-ctx.strokeStyle = "sienna"; // Définition de la couleur de contour
-ctx.lineWidth = 5;         // Définition de la largeur de ligne
-ctx.fill();            // Application du remplissage
-ctx.stroke();          // Application du contour
-
-// Mât
-ctx.beginPath();
-ctx.moveTo(140,50);
-ctx.lineTo(140,250);
-ctx.lineWidth = 10;
-ctx.stroke();
-});
-
+window.onload = function() {
+	var sens = -1;
+	
+	// Transition avec fonction de rappel
+	function transition() {
+		alert('ok');
+		sens *= -1;
+		pendule.transitionTo({
+			rotation: sens * Math.PI / 4, 
+			duration: 2,
+			easing: "ease-in-out",
+			callback: function() {transition();}
+		});		
+	}
+	
+	// Scène
+	var scène = new Kinetic.Stage({
+	  container: "kinetic",
+	  width: 500,
+	  height: 300
+	});
+	
+	var calque = new Kinetic.Layer();
+	
+	// Construction du pendule avec un groupe
+	var pendule = new Kinetic.Group( {
+		x: 250,
+		y: 50
+	});
+	var boule = new Kinetic.Ellipse({
+		x: 0,
+		y: 180,
+		radius: 20,
+		fill: "blue"
+	});
+	var fil = new Kinetic.Line({
+		points: [0, 0, 0, 180],
+		stroke: "black"
+	});
+	
+	pendule.add(fil);
+	pendule.add(boule);
+	calque.add(pendule);
+	scène.add(calque);
+	
+	transition();
+};
